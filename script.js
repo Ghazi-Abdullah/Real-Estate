a82c138072b60d20e8d07a502fb973527b4855ef
Ghazi-Abdullah <155458342+Ghazi-Abdullah@users.noreply.github.com>
2025-04-15 15:45:55 +0300

    Change the main color and re-desghin the pages

--- script.js
+++ script.js
@@ -28,3 +28,33 @@ window.onscroll = () =>{
             headings.parentElement.classList.toggle('active');
         }
     });
+
+
+    // Get the span element
+let dateAndTime = document.querySelector('.listing-date');
+
+// Get the current date and time
+let date = new Date();
+
+// Format the date and time (e.g., "2025-04-13 14:30:00")
+/*let formattedDate = date.getFullYear() + '-' +
+    String(date.getMonth() + 1).padStart(2, '0') + '-' +
+    String(date.getDate()).padStart(2, '0') + ' ' +
+    String(date.getHours()).padStart(2, '0') + ':' +
+    String(date.getMinutes()).padStart(2, '0') + ':' +
+    String(date.getSeconds()).padStart(2, '0');**/
+
+// Set the formatted date and time in the span
+dateAndTime.textContent = date.toLocaleString('en-GB', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    //hour: '2-digit',
+    //minute: '2-digit',
+    //second: '2-digit',
+    //hour12: false
+});
+
+
+
+    
